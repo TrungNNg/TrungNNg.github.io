bf03c5d5d427d8510c306110fdf79862798f7862
Trung Nguyen <nhattrungnguyen14@yahoo.com>
2023-08-11 23:33:03 -0400

    test

--- code/test.go
+++ code/test.go
@@ -2,80 +2,41 @@ package main
 
 import (
     "fmt"
-    ""
+    "sync"
 )
-var p = fmt.Println
+
 func main() {
-    arr := []int{1,2,2}
-    p(subsets(arr))
+    sum := 0
+
+    var wg sync.WaitGroup
+    var mu sync.Mutex
+
+    wg.Add(1)
+    go foo(&wg, &sum, &mu)
+
+    wg.Add(1)
+    go bar(&wg, &sum, &mu)
+
+    wg.Wait()
+    fmt.Println("sum: ", sum)
 }
 
-// generate all subset
-func subsets(nums []int) [][]int {
-    ans := [][]int{}
-    //m := map[int]bool{}
-    subsets_helper(&ans, nums, []int{})
-    return ans
-}
-
-func subsets_helper(ans *[][]int, remain, current []int) {
-    if len(remain) == 0 {
-        *ans = append(*ans, append([]int{}, current...))
-        return
-    }
-    current = append(current, remain[0])
-    subsets_helper(ans, remain[1:], current)
-    current = current[:len(current)-1]
-    subsets_helper(ans, remain[1:], current)
-}
-
-// permute
-func permute(nums []int) [][]int {
-    ans := [][]int{}
-    m := map[int]bool{}
-    permute_helper(&ans, nums, []int{}, m)
-    return ans
-}
-
-func permute_helper(ans *[][]int, remain, current []int, chosen map[int]bool) {
-    if len(current) == len(remain) {
-        *ans = append(*ans, append([]int{}, current...))
-        return
-    }
-    for i := range remain {
-        if !chosen[remain[i]] {
-            current = append(current, remain[i])
-            chosen[remain[i]] = true
-            permute_helper(ans, remain, current, chosen)
-            current = current[:len(current)-1]
-            chosen[remain[i]] = false
-        }
+func foo(wg *sync.WaitGroup, sum *int, mu *sync.Mutex) {
+    defer wg.Done()
+    defer mu.Unlock()
+    mu.Lock()
+    for i := 0; i < 100000; i++ {
+        *sum += 1
     }
 }
 
-// combination
-func combine(n int, k int) [][]int {
-    ans := [][]int{}
-    remain := []int{}
-    for i := 1; i <= n; i++ {
-        remain = append(remain, i)
-    }
-    combine_helper(&ans, remain, []int{}, k, 0)
-    return ans
-}
-
-func combine_helper(ans *[][]int, remain, current []int, k int, index int) {
-    if len(current) == k {
-        *ans = append(*ans, append([]int{}, current...))
-    } else {
-        for i := index; i < len(remain); i++ {
-            current = append(current, remain[i])
-            combine_helper(ans, remain, current, k, i+1)
-            current = current[:len(current)-1]
-        }
+func bar(wg *sync.WaitGroup, sum *int, mu *sync.Mutex) {
+    defer wg.Done()
+    defer mu.Unlock()
+    mu.Lock()
+    for i := 0; i < 100000; i++ {
+        *sum += 1
     }
 }
-
-
 
 
